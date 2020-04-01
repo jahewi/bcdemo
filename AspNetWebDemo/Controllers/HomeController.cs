3c49fc5d6f1fc09aa60ea44bcb53786195e63a8f
Henrik Wilson <j.henrik.wilson@gmail.com>
2020-04-01 13:04:26 +0300

    Added About page.

--- AspNetWebDemo/Controllers/HomeController.cs
+++ AspNetWebDemo/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AspNetWebDemo.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
